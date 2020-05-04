12b26bc261c5a885709839d158faafaabdf26b2c
mos <mosos913@hotmail.com>
2020-05-04 17:52:56 +0700

    question app

--- src/App.js
+++ src/App.js
@@ -1,51 +1,69 @@
 import React, { useState } from 'react'
-import { Button } from 'reactstrap'
+import {
+	Button,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	Container,
+} from 'reactstrap'
 import { myQuestions } from './data'
 import { AnswerOption, AnswerCheck } from './components'
 
 export const App = () => {
-	const [data, setData] = useState(myQuestions)
-	const [value, setValue] = useState({})
+	const [answer, setValue] = useState({})
 	const [isCorrect, setIsCorrect] = useState([])
 	const [checked, isChecked] = useState(false)
 	const [validate, setValidate] = useState(false)
+	const [isToggle, setIsToggle] = useState(false)
+	const [allCorrect, setAllCorrect] = useState(0)
+
+	const toggle = () => setIsToggle(!isToggle)
 
 	const handleChange = (e) => {
+		const { name, value } = e.target
 		setValue({
-			...value,
-			[e.target.name]: e.target.value,
+			...answer,
+			[name]: value,
 		})
 	}
 
 	const checkAnswer = () => {
-		if (checkValidate()) {
-			const correctAnswerList = data.map((e) => e.correctAnswer)
-			const answers = Object.values(value)
+		const correctAnswerList = myQuestions.map((e) => e.correctAnswer)
+		const answers = Object.values(answer)
+		const dataList = myQuestions.map((e) => e.question)
+		const validList = []
+		toggle()
+		for (let i in dataList) {
+			if (answers[i] === undefined) {
+				validList.push(true)
+				setValidate(true)
+			} else {
+				validList.push(false)
+				setValidate(false)
+			}
+		}
+		const valid = validList.every(isValidateAll)
+		if (valid) {
 			const checkAnswer = []
-			for (let i = 0; i < data.length; i++) {
+			for (let i = 0; i < myQuestions.length; i++) {
 				const answer = parseInt(answers[i])
 				correctAnswerList[i] === answer
 					? checkAnswer.push(1)
 					: checkAnswer.push(0)
 			}
 			setIsCorrect(checkAnswer)
+			setAllCorrect(checkAnswer.reduce((acc, curr) => acc + curr))
 			isChecked(true)
 		}
 	}
 
-	const checkValidate = () => {
-		const questionList = data.map((e) => e.question)
-		const keys = Object.keys(questionList)
-		console.log(value)
-		for (let i = 0; i < data.length; i++) {
-			
-		}
-	}
+	const isValidateAll = (currentValue) => currentValue === false
 
 	return (
-		<div>
+		<Container>
 			<ol>
-				{data.map((e, i) => {
+				{myQuestions.map((e, i) => {
 					return (
 						<div key={i}>
 							<li>
@@ -56,7 +74,7 @@ export const App = () => {
 								<AnswerOption
 									onChange={handleChange}
 									answers={e.answers}
-									value={value || null}
+									value={answer}
 									name={e.question}
 									disabled={checked}
 								/>
@@ -65,9 +83,31 @@ export const App = () => {
 					)
 				})}
 			</ol>
-			<Button onClick={checkAnswer} outline color='success'>
+			<Button
+				onClick={checkAnswer}
+				outline
+				color='success'
+				style={{ marginLeft: '40px' }}
+			>
 				Submit
 			</Button>
-		</div>
+			<Modal isOpen={isToggle} toggle={toggle}>
+				<ModalHeader toggle={toggle}>
+					{validate ? 'Some choices were required' : 'How many correct answer?'}
+				</ModalHeader>
+				<ModalBody>
+					{validate ? (
+						'Please answer all questions.'
+					) : (
+						<span>You got all {allCorrect} correct answers.</span>
+					)}
+				</ModalBody>
+				<ModalFooter>
+					<Button color='primary' onClick={toggle}>
+						Ok
+					</Button>
+				</ModalFooter>
+			</Modal>
+		</Container>
 	)
 }
